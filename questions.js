ecd770c159da25520e07d6827c91c517a1ad306b
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-12-26 21:59:27 -0800

    Adding script and questions js files

---
+++ questions.js
@@ -0,0 +1,31 @@
+const questions [
+    {
+	question: "What is the approximate cruising altitude of most commercial aircraft?",
+	options: ["A. 10 000 ft", "B. 30 000 ft", "C. 50 000 ft", "D. 70 000 ft"],
+	correctAnswer: "B",
+    },
+
+    {
+	question: "What aircraft is the world's largest passenger aircraft?",
+	options: ["A. Boeing 747", "B. Airbus A380", "C. Concorde", "D. Boeing 777"],
+	correctAnswer: "B",
+    },
+
+    {
+	question: "What does the black box in an aircraft record?",
+	options: ["A. Speed", "B. Altitude", "C. Flight data and cockpit audio", "D. Passenger details"],
+	correctAnswer: "C",
+    },
+
+    {
+	question: "What is the primary function of an aircraft's wing flaps?",
+	options: ["A. Increase speed", "B. Reduce noise", "Stabilize flight", "D. Assist during takeoff and landing"],
+	correctAnswer: "D",
+    },
+
+    {
+	question: "What is the role of the rudder in an aircraft?",
+	options: ["A. Control yaw", "B. Control pitch", "C. Control roll", "D. Increase speed"],
+	correctAnswer: "A",
+    },
+];
