2e60c894782fbf2b24c4336c6a8b6087584fa78d
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2025-01-01 10:31:42 -0800

    Updating the Continue and Exit buttons

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.pop-info');
+const continueBtn = document.querySelector('.continue-btn');
+const exitBtn = document.querySelector('.exit-btn');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
+}
+
+continueBtn.onclick = () => {
+    popupInfo.classList.add('active');
+}
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
 }
