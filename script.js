366a62e9ad6bb53c97c88f47a6905bbca873e8c5
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2025-01-16 00:20:48 -0800

    Adding shuffling for questions

--- script.js
+++ script.js
@@ -163,9 +163,17 @@ let questionCount = 0;
 let score = 0;
 const userAnswers = Array(questions.length).fill(null);
 
+function shuffleQuiz(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j] = array[j], array[i]];
+    }
+};
+
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
     landing.classList.add('active');
+    shuffleQuiz(questions);
 };
 
 backBtn.onclick = () => {
